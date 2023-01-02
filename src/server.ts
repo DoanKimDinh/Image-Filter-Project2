dce1592678f8c009de92dac62134da156c593ab5
DoanKimDinh <doankimdinh113@gmail.com>
2023-01-03 02:57:14 +0700

    project2 udacity

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { filterImage, deleteFiles } from "./util";
+
+(async () => {
+  const app = express();
+  const port = process.env.PORT || 8081;
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const image_url = req.query.image_url;
+    let result: string = null;
+
+    if (!image_url) {
+      return res.status(400).send({ message: "URL is required" });
+    }
+
+    try {
+      result = await filterImage(image_url);
+    } catch (error) {
+      return res.status(422).send({ message: "Can not filter" });
+    }
+
+    if (!result) {
+      return res.status(404).send({ message: "Not found" });
+    }
+
+    return res.status(200).sendFile(result, () => {
+      deleteFiles([result]);
+    });
+  });
+
+  app.get("/", async (req: Request, res: Response) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+  });
+})();
---
+++ src/util/index.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import Jimp = require("jimp");
+
+export async function filterImage(inputURL: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const image = await Jimp.read(inputURL);
+      const pathImage = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await image
+        .resize(256, 256)
+        .quality(60)
+        .greyscale()
+        .write(__dirname + pathImage, (img) => {
+          resolve(__dirname + pathImage);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+export async function deleteFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
